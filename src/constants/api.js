1bc91af7e3d71dc07a2b60a35f6c90494a9d1a97
weii1501 <vodacluc15@gmail.com>
2023-12-26 21:11:58 +0700

    api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,5 @@
 export const BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000'
+// export const USER_API = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/user/'
 
 export const COURSE_API = `${BASE_URL}/api/courses/`
 export const MY_COURSE_API = `${BASE_URL}/api/courses/my-courses/`
