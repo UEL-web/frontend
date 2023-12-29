6eb1f92657d00399772c8bf0db4e058c8e98dcf8
weii1501 <vodacluc15@gmail.com>
2023-12-29 22:45:59 +0700

    fix ui question

--- src/components/home/CardQuestion.js
+++ src/components/home/CardQuestion.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {HiPlus} from "react-icons/hi";
+import {HiMinus, HiPlus} from "react-icons/hi";
 
 export default function CardQuestion ({ question, answer }) {
     const [open, setOpen] = useState(false)
@@ -9,11 +9,13 @@ export default function CardQuestion ({ question, answer }) {
         <div className='relative mb-4 '>
             <p
                 className='relative p-4 text-[16px] card-question cursor-pointer z-10'
-                onClick={() => setOpen(!open)}
+                // onClick={() => setOpen(!open)}
+                onMouseOver={() => setOpen(true)}
+                onMouseOut={() => setOpen(false)}
             >
                 {question}
                 <span className='absolute right-3 top-[50%] transform -translate-y-1/2 '>
-                    <HiPlus />
+                    {!open ? <HiPlus /> : <HiMinus />}
                 </span>
             </p>
             {open && <p className='p-4 rounded-[14px] border-[#F74986] border-[1px] text-white bg-transparent hover:text-black hover:bg-white transition duration-100 ease-in-out mt-1'>
